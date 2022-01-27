ad41fac712ba60e5d898def3408b38920ff22c2b
Antz-Lee <afrancois2@umassd.edu>
2022-01-27 00:49:16 -0500

    matlab save

--- Distance Sensor/MATLAB/hcsr04SensorReading.m
+++ Distance Sensor/MATLAB/hcsr04SensorReading.m
@@ -1,17 +1,25 @@
-
+clear all
 %setting arduino to port output from pc 
-AU = arduino('com4','uno', 'Libraries', 'JRodrigoTech/HCSR04' ); 
-% Creating Ultrasonic sensor with trigger pin D2 & Echo pin D3
-sensor = addon(AU, 'JRodrigoTech/HCSR04', 'D2', 'D3'); 
+AU = arduino('COM7','Uno', 'Libraries', 'Ultrasonic' );
+
+
+% Creating Ultrasonic sensor with trigger pin D12 & Echo pin D13
+sensor = ultrasonic(AU,'D12', 'D13');  
 
 % Measuring Time 
-t = readTravelTime(sensor); 
-distCM = (0.340.*t)/2; 
-sprintf('Current Distance in cm: %.2f \n', distCM);
+%t = readTravelTime(sensor); 
+%distCM = (0.340.*t)/2 
+%sprintf('Current Distance in cm: %.2f \n', distCM)
 
 % Measuring Distance 
-DistRead = readDistance(sensor); 
-sprintf('Distance in cm: %.2f \n', DistRead); 
+DistRead = readDistance(sensor);  %Read in meters 
 
-clear sensor ; 
-clear a;
+%Distance conversion to cm and inch
+cmDist = DistRead *100;    
+inchDist = DistRead *39.3700787;
+fprintf('Distance in cm: %.2f \n', DistRead);
+fprintf('Distance in cm: %.2f \n', cmDist);
+fprintf('Distance in cm: %.2f \n', inchDist);
+%clear sensor ; 
+%clear AU;
+%}
