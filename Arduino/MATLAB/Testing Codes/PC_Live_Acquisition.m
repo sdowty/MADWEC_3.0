1c0d3fb932dfe5765fc7f122e379e3ec92ca0ab9
Antz-Lee <afrancois2@umassd.edu>
2022-04-25 17:41:44 -0400

    PC Version

--- Arduino/MATLAB/Testing Codes/PC_Live_Acquisition.m
+++ Arduino/MATLAB/Testing Codes/PC_Live_Acquisition.m
@@ -1,13 +1,22 @@
 %% MADWEC 3.0 - Live Data Acquisition
+% Script uses an Arduino (Uno) microcontroller to test an array of sensors with such as:
+% (1) Current Sensor (10A MAX),(2) Voltage Sensor (11x attenuation),(3) HC- SR04 Distance Sensor 
+% The script plots live data from the sensors until a stop button (s) is pressed using 
+% the key board. After the live acquisition, post procress functions measures Velocity
+% and Output Power. The script creates a main folder that saves test runs in sub folders 
+% that contains the following:
+% (1) saved data on a excel spreadsheet, (2) saved figures, (3) Comment Text File 
+
 
 %% Close all open figures
 clear all 
 clear;clc;
 close ALL 
 delete(instrfindall);
+
 %% Initialize Test Parameters
     fprintf('-------Initialize Testing Parameters-------\n');
-    fprintf('***After wave height..WAIT TILL WINCH MOVES! then Press Enter** \n');
+    fprintf('***After wave height..WAIT TILL WINCH MOVES! then Press Enter*** \n');
     
     testPrompt = 'Test number (#): ';
     powPrompt = 'Winch Power (%): ';   
@@ -21,20 +30,19 @@ delete(instrfindall);
     T = num2str(TestInput);
     P = num2str(PowInput);
     H = num2str(WavInput);
-% date = datestr(now,'mm.dd');
-% DataFolder = strcat(date,' P', P,' W',H,' Test - ',T);
-% Folder Name Ex: '04.05_P15_W0.66_Test - 1'
+
+    % Folder Name Output: '04.05_P15_W0.66_Test - 1'
 
 %% Team COM PORTS
-    % Marina Mac COM Port 
+    % Mac/Os COM Port - Marina
     % Distance Sensor - '/dev/cu.usbmodem1301'
     % Current Sensor - '/dev/cu.usbmodem1101'
 
-    % Antz-Lee COM PORT
+    % PC/Windows COM PORT - Antz-Lee 
     % Distance Sensor - 'COM9'
     % Current Sensor - 'COM8' 
 
-    % Spencer COM PORT
+    % PC/Windows COM PORT - Spencer
     % Distance Sensor - 'COM4'
     % Current Sensor - 'COM3' 
     
@@ -46,10 +54,10 @@ delete(instrfindall);
 % change the COM PORT
 
     % Distance Sensor 
-        arduinoObj = arduino('/dev/cu.usbmodem1301','Uno','Libraries','Ultrasonic');
+        arduinoObj = arduino('COM9','Uno','Libraries','Ultrasonic');
         ultrasonicObj = ultrasonic(arduinoObj,'D9','D10'); % trig D9 %echo D10
     % Current Sensor 
-        currentSM = serial('/dev/cu.usbmodem1101','BaudRate',9600);
+        currentSM = serial('COM8','BaudRate',9600);
         fopen(currentSM); 
 %% Acquiring and Displaying Live Data: (Distance, Voltage)
 
@@ -92,9 +100,16 @@ delete(instrfindall);
 
     % Setting start time for live plot
         startTime = datetime('now');
-
-% Distance Readings
-    while (strcmp(key, 's') == 0 ) % Press 'S' Key to STOP Live Acquisition
+    
+    % Acquisition time (min). Insert inf to disable time limit.
+        waitTime = 2;
+        waitTime = duration(0,waitTime,0);
+        
+    % Starting time for Clock   
+        t = datetime('now') - startTime;
+        
+% Live Acquisition Readings
+    while ((strcmp(key, 's') == 0 ) && t < waitTime ) % Press 'S' Key to STOP Live Acquisition
      % get the key currently pressed
         key = get(gcf,'CurrentKey'); 
 
@@ -114,9 +129,9 @@ delete(instrfindall);
         addpoints(v,datenum(t),voltage)  % Voltage 
         addpoints(c,datenum(t),current)  % Current
      % Update axes
-        dx.XLim = datenum([t-seconds(15) t]); 
-        vx.XLim = datenum([t-seconds(15) t]);
-        cx.XLim = datenum([t-seconds(15) t]);
+        dx.XLim = datenum([t-seconds(20) t]);   
+        vx.XLim = datenum([t-seconds(20) t]);
+        cx.XLim = datenum([t-seconds(20) t]);
         datetick('x','keeplimits')
         datetick('x','keeplimits')
         datetick('x','keeplimits')
@@ -127,7 +142,6 @@ delete(instrfindall);
 % Output message
     if (strcmp(key, 's') == 0 )
         disp('\n ***Live Data acquisition ended because the TIME limit has been reached!***')
-
     else
         disp('\n ***Live Data acquisition ended because the STOP button has been pressed!***')
 
@@ -166,7 +180,8 @@ delete(instrfindall);
         T2 = table(timeP,Output_Power, 'VariableNames',{'time','Output Power'});
 
 %% Plotting Total Data Aqcuistion 
-
+    % Minimize figure 1 
+        f1.WindowState = 'minimized'; 
     % Setting Second Figure
         f2 = figure('Name', 'Total Distance & Velocity Plot');
         f2.WindowState = 'maximized';
@@ -229,11 +244,12 @@ delete(instrfindall);
     % Folder Name Ex: '04.05 P15 W0.66 Test - 1'
 
     % Establishing Current Folder Location
-        CurrentFolder = '/Users/marinameehan/Downloads/MATLAB 2022' ;% Input Desired/Current Folder Location
+        CurrentFolder = 'C:\Users\3cityDELL\OneDrive\Documents\MADWEC_3.0\Arduino\MATLAB2' ;% Input Desired/Current Folder Location
         MsDataFolder = 'Data Acquistion';
         MsFolderLoc = fullfile(CurrentFolder,MsDataFolder); 
         SvFolderLoc = fullfile(MsFolderLoc,SvDataFolder);
 
+    
 % If the folder exist, Save file in it
     if exist(MsFolderLoc, 'dir') 
         % Creating Folder Directory
@@ -242,7 +258,12 @@ delete(instrfindall);
             writetable(Final_T, fullfile(SvFolderLoc,filename))
         % Saving Figure to folder Location
             saveas(figure(2), fullfile(SvFolderLoc,'Total Data Aqcuisation Screen Shot'), 'png')
-
+            
+        % Creating Comments Folder Location
+            comments = fullfile(SvFolderLoc, 'Comments.txt');
+        % Saving Comments 
+            fopen(comments,'w');
+            
         % Print Data confirmation to command line
             fprintf('\nFolder Location: "C:..../%s" was found in directory. \n',SvDataFolder);
             fprintf('Resulting Table: \n %g Measurements of Distances, Velocity, Voltage, Current, and Output Power \n',...
@@ -257,8 +278,14 @@ delete(instrfindall);
             writetable(Final_T, fullfile(SvFolderLoc,filename))
         % Saving Figure to folder Location
             saveas(figure(2), fullfile(SvFolderLoc,'Total Data Aqcuisation Screen Shot'), 'png')
+            
+        % Creating Comments Folder Location
+            comments = fullfile(SvFolderLoc, 'Comments.txt');
+        % Saving Comments 
+            fopen(comments,'w');
+            
         % Print Folder Location confirmation to command line
-            fprintf('\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! \n')
+            fprintf('\n !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! \n')
             fprintf('Folder: "C:.../%s" was NOT found in directory, so a new folder: "%s" was created! \n',...
             MsDataFolder,MsDataFolder);
             fprintf('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! \n\n')
